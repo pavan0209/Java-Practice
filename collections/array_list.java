e43458dad1b7f53ed06af8af35e5a1d4647cd950
pavan0209 <pavanasonawane@gmail.com>
2026-01-10 11:52:30 +0530

    array list

---
+++ collections/array_list.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(19);
+        list.add(20);
+        list.add(5);
+
+        System.out.println(list);
+    }
+}
