13b5102e609a9292f64a9675eae6185b1b53a900
pavan0209 <pavanasonawane@gmail.com>
2026-02-22 10:50:52 +0530

    find transaction sum by day

---
+++ streams/16_trasaction_sum_by_day.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Transaction {
+    private String date;
+    private int amount;
+
+    Transaction(String date, int amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction [date=" + date + ", amount=" + amount + "]";
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction("2022-01-01", 100),
+                new Transaction("2022-01-01", 200),
+                new Transaction("2022-01-02", 300),
+                new Transaction("2022-01-02", 400),
+                new Transaction("2022-01-03", 500));
+
+        Map<String, Integer> sumByDay = transactions.stream().collect(Collectors.groupingBy  (t -> t.getDate(), Collectors.summingInt(t-> t.getAmount())))
+    }
+}
---
+++ streams/17_kth_smallest.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(1, 4, 10, 5, 6, 8, 11, 2, 7);
+        int k = 9;
+
+        int res = list.stream()
+                .distinct()
+                .sorted()
+                .skip(k - 1)
+                .findFirst()
+                .orElse(-1);
+
+        System.out.println(res);
+
+        int[] arr = { 1, 4, 10, 5, 6, 8, 11, 2, 7 };
+
+        Arrays.stream(arr)
+                .sorted()
+                .skip(k - 1)
+                .findFirst()
+                .ifPresent(num -> System.out.println(num));
+    }
+}
