bda9529e9f6090611cb21cfbf24d8dcda5018e19
pavan0209 <pavanasonawane@gmail.com>
2026-02-28 18:04:05 +0530

    left aligned number triangle

---
+++ patterns/p01_pattern.java
@@ -0,0 +1,40 @@
+package patterns;
+
+import java.util.Scanner;
+
+/*
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+*/
+
+class Solution {
+
+    static void printPattern(int rows) {
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        printPattern(rows);
+
+        sc.close();
+    }
+}
